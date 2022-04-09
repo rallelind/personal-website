6fbf3ecdc199722c3077ea2c8993a5228dcb2cb9
Rasmus Lind <rasmus1234555@hotmail.dk>
2022-04-09 10:27:02 +0200

    made changes to custom hook

--- pages/projects.js
+++ pages/projects.js
@@ -3,11 +3,11 @@ import MernText from "../components/Projects/MernText"
 import SoMeCard from "../components/Projects/SoMeCard"
 import SoMeText from "../components/Projects/SoMeText"
 import { Grid } from "@nextui-org/react"
-import { useMediaQuey } from "../lib/custom-hooks/useMediaQuery"
+import { useMediaQuery } from "../lib/custom-hooks/useMediaQuery"
 
 const projects = () => {
 
-    const md = useMediaQuey(960)
+    const md = useMediaQuery(960)
 
     return (
         <Grid.Container gap={3} justify="center">
